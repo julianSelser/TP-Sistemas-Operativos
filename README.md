6b5d319809877d268a7f3349f9845bbf416c2674
utnso <utnso@utnso>
2013-04-21 23:09:02 -0300

    se hizo el dificil

---
+++ README.md
@@ -0,0 +1,4 @@
+tp-20131c-juanito-y-los-clonosaurios
+====================================
+
+Implementacion del TP de Sistemas Operativos del grupo Juanito y los clonosaurios
---
+++ prueba.c
@@ -0,0 +1,18 @@
+/*
+ * prueba.c
+ *
+ *  Created on: 19/04/2013
+ *      Author: utnso
+ */
+
+#include <stdio.h>
+
+int main()
+
+
+{
+	puts("Hola grupo!!!\n");
+    printf("hola gente\n");
+
+	return 0;
+}
