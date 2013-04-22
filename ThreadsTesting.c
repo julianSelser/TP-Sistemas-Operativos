67e0abd063e5659a58fabda0ba1a5c9cb1b48ec3
dcappellini <dcappellini@hotmail.com.ar>
2013-04-22 00:25:04 -0300

    Agrego el archivo de los threads productor-consumidor. Falta cambiar el tipo int por uno independiente de la arquitectura.

---
+++ ThreadsTesting.c
@@ -0,0 +1,32 @@
+/*
+ * ThreadsTesting.c
+ *
+ *  Created on: 21/04/2013
+ *      Author: utnso
+ */
+/*
+ * Archivo de pruebas para POSIX threads.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t semaforo;
+
+int main()
+{
+	//el hilo principal debería lanzar los otros dos hilos
+	return 0;
+}
+
+int productor()
+{
+	return 0;
+}
+
+int consumidor()
+{
+	return 0;
+}
