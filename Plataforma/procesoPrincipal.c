bdb6188a45f371b6cd66d2c347cf71a74597e535
dcappellini <dcappellini@hotmail.com.ar>
2013-04-27 02:07:43 -0300

    Reorganizacion general del repositorio.

---
+++ Plataforma/procesoPrincipal.c
@@ -0,0 +1,47 @@
+/*
+ * procesoPrincipal.c
+ *
+ *  Created on: 26/04/2013
+ *      Author: utnso
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+
+pthread_mutex_t semaforo;
+
+void * rutina_productor();
+void * rutina_consumidor();
+
+int8_t shared_var;
+
+
+
+int main()
+{
+	pthread_t Productor, Consumidor;
+	char n_productor[20] = "Thread_Productor";
+	char n_consumidor[20] = "Thread_Consumidor";
+	int16_t r_prod, r_cons;
+
+	r_prod = pthread_create(&Productor, NULL, rutina_productor, (void *) n_productor);
+	r_cons = pthread_create(&Consumidor, NULL, rutina_consumidor, (void *) n_consumidor);
+	//el hilo principal debería lanzar los otros dos hilos
+	while(1);
+	return 0;
+}
+
+void * rutina_productor()
+{
+	while(1) puts("Ola\n");
+	return 0;
+}
+
+void * rutina_consumidor()
+{
+	while(1) puts("Ke ase\n");
+	return 0;
+}
