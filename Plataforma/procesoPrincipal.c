81de2358a6ae70305219a3d98778441e57e6b844
dcappellini <dcappellini@hotmail.com.ar>
2013-04-28 16:39:19 -0300

    Ahora el productor pide un string por teclado, y el consumidor lo devuelve por pantalla. Se sincronizan con un par de semáforos cruzados.

--- Plataforma/procesoPrincipal.c
+++ Plataforma/procesoPrincipal.c
@@ -10,14 +10,19 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <log.h>
+#include <unistd.h>
 
-pthread_mutex_t semaforo;
+#define BUFFER_SIZE 20
+
+pthread_mutex_t habilitar_productor = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t habilitar_consumidor = PTHREAD_MUTEX_INITIALIZER;
 
 void * rutina_productor();
 void * rutina_consumidor();
 
-int8_t shared_var;
-
+int8_t shared_var = 0;
+char shared_buffer[BUFFER_SIZE];
 
 
 int main()
@@ -25,25 +30,57 @@ int main()
 	pthread_t Productor, Consumidor;
 	char n_productor[20] = "Thread_Productor";
 	char n_consumidor[20] = "Thread_Consumidor";
-	int16_t r_prod, r_cons;
+	int r_prod, r_cons;
+	t_log * logger_plataforma;
+
+	logger_plataforma = log_create("plataforma.log", "Plataforma", 1, LOG_LEVEL_TRACE);
+
+
+
+	pthread_mutex_lock(&habilitar_consumidor); //el consumidor empieza bloqueado
 
 	r_prod = pthread_create(&Productor, NULL, rutina_productor, (void *) n_productor);
+	log_debug(logger_plataforma, "Se lanza el hilo productor!", "DEBUG");
+
 	r_cons = pthread_create(&Consumidor, NULL, rutina_consumidor, (void *) n_consumidor);
+	log_debug(logger_plataforma, "Se lanza el hilo consumidor!", "DEBUG");
 	//el hilo principal debería lanzar los otros dos hilos
-	while(1);
+
+	pthread_join(Productor, NULL);
+	pthread_join(Consumidor, NULL);
+
+	log_destroy(logger_plataforma);
+
 	return 0;
 }
 
-// por ahora pruebo que se lancen bien los hilos...
 
 void * rutina_productor()
 {
-	while(1) puts("Hola\n");
+
+	while (1)
+	{
+		pthread_mutex_lock(&habilitar_productor);
+		puts("ingrese el texto: ");
+		fgets(shared_buffer, BUFFER_SIZE, stdin);
+		puts("\n");
+		pthread_mutex_unlock(&habilitar_consumidor);
+		sleep(1);
+	}
+
 	return 0;
+
 }
 
 void * rutina_consumidor()
 {
-	while(1) puts("Que tal\n");
+	sleep(1);
+	while(1)
+	{
+		pthread_mutex_lock(&habilitar_consumidor);
+		printf("Se recibió el texto: %s", shared_buffer);
+		pthread_mutex_unlock(&habilitar_productor);
+		sleep(1);
+	}
 	return 0;
 }
