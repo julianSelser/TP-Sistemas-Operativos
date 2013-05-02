48bae2f5b22566c5f8d9f2e1488c939eb6316b76
julian <julianSelser@yahoo.com.ar>
2013-05-02 13:38:21 -0300

    borrado el archivo de proyecto de TP JUANITO

---
+++ memoria/Debug/memoria.d
@@ -0,0 +1,9 @@
+memoria.d: ../memoria.c ../memoria.h \
+ /home/julian/git/tp-20131c-juanito-y-los-clonosaurios/so-commons-library/src/commons/collections/list.h \
+ /home/julian/git/tp-20131c-juanito-y-los-clonosaurios/so-commons-library/src/commons/collections/node.h
+
+../memoria.h:
+
+/home/julian/git/tp-20131c-juanito-y-los-clonosaurios/so-commons-library/src/commons/collections/list.h:
+
+/home/julian/git/tp-20131c-juanito-y-los-clonosaurios/so-commons-library/src/commons/collections/node.h:
---
+++ so-nivel-gui-library/Debug/nivel-gui/nivel.d
@@ -0,0 +1,3 @@
+nivel-gui/nivel.d: ../nivel-gui/nivel.c ../nivel-gui/nivel.h
+
+../nivel-gui/nivel.h:
