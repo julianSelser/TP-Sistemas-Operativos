b0dc0452c773bc3df083b45a94577a07df121389
dcappellini <dcappellini@hotmail.com.ar>
2013-06-08 14:53:05 -0300

    Agrego archivos (vacíos) para el hilo que escucha conexiones.

---
+++ Nivel/hiloEscuchaConexiones.c
@@ -0,0 +1,8 @@
+/*
+ * hiloEscuchaConexiones.c
+ *
+ *  Created on: 08/06/2013
+ *      Author: utnso
+ */
+
+
---
+++ Nivel/hiloEscuchaConexiones.h
@@ -0,0 +1,13 @@
+/*
+ * hiloEscuchaConexiones.h
+ *
+ *  Created on: 08/06/2013
+ *      Author: utnso
+ */
+
+#ifndef HILOESCUCHACONEXIONES_H_
+#define HILOESCUCHACONEXIONES_H_
+
+
+
+#endif /* HILOESCUCHACONEXIONES_H_ */
