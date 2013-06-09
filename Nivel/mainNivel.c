de8e28a262daed992faf248abb737dabc8eb164c
dcappellini <dcappellini@hotmail.com.ar>
2013-06-08 23:17:25 -0300

    main actualizado para incluir definicion de las variables extern correspondientes.

--- Nivel/mainNivel.c
+++ Nivel/mainNivel.c
@@ -24,6 +24,18 @@
 //hilo principal del proceso nivel
 //lee el archivo de configuración, crea el logger y lanza los demás hilos
 //tambien inicializa los contadores de recursos
+	char * nombre;
+	char * ip_orquestador;
+	int puerto_orquestador;
+	int tiempo_chequeo_deadlock;
+	int recovery;
+	uint8_t cantidad_de_recursos;
+
+
+	t_log * logger;
+
+	t_list * lista_cajas;
+
 
 int conf_es_valida(t_config * configuracion);
 int imprimir_nodo_caja(t_caja * nodo_caja);
@@ -32,6 +44,9 @@ int main(int argc, char ** argv)
 {
 	t_config * configuracion;
 	char * log_name;
+	char ** ip_puerto_separados;
+	char * ip_puerto_orquestador;
+	char * temp_ip_puerto_orq;
 
 	int i;
 
@@ -58,8 +73,19 @@ int main(int argc, char ** argv)
 	string_append(&log_name, ".log");
 	logger = log_create(log_name, "NIVEL", 0, LOG_LEVEL_TRACE);
 
+	//podra el personaje reutilizar este codigo?
+	temp_ip_puerto_orq = config_get_string_value(configuracion, "orquestador");
+	ip_puerto_orquestador = malloc(strlen(temp_ip_puerto_orq));
+	strcpy(ip_puerto_orquestador, temp_ip_puerto_orq);
+	ip_puerto_separados = string_split(ip_puerto_orquestador, ":");
+	free(ip_puerto_orquestador);
 
-	ip_puerto_orquestador = config_get_string_value(configuracion, "orquestador");
+	ip_orquestador = malloc(strlen(ip_puerto_separados[0]));
+	strcpy(ip_orquestador, ip_puerto_separados[0]);
+	puerto_orquestador = atoi(ip_puerto_separados[1]);
+	free(ip_puerto_separados);
+
+
 	tiempo_chequeo_deadlock = config_get_int_value(configuracion, "TiempoChequeoDeadlock");
 	recovery = strcmp("On", config_get_string_value(configuracion, "Recovery"));
 
@@ -123,11 +149,4 @@ int imprimir_nodo_caja(t_caja * nodo_caja)
 {
 	printf("Esta caja contiene %d %s de %d, su símbolo es %c y se encuentra en la posición (%d,%d)\n", nodo_caja->disp, nodo_caja->nombre, nodo_caja->disp, nodo_caja->ID, nodo_caja->x, nodo_caja->y);
 	return 0;
-}
-
-
-
-
-//--------------FIN MAIN--------------//
-
 }
