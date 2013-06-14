2b5b7b67fc17d5aff98540f61e499a66560724bb
julian <julianSelser@yahoo.com.ar>
2013-06-13 21:43:32 -0300

    serializadora totalmente funcional, probada en la vm "server"

--- so-serial-library/serial.c
+++ so-serial-library/serial.c
@@ -23,12 +23,15 @@
 // falta setear los sockets para cerrarse con execve
 #define N_MENSAJES 20 //por ahora son 20
 
+
 static p_funcion_deserial vec_deserializador[N_MENSAJES];
 static p_funcion_serial vec_serializador[N_MENSAJES];
 
 
+
 /******************************************* FUNCIONES DE INICIALIZADO ********************************************/
 /*									ESTO ES TEMPORAL; HAY UNA MEJOR MANERA DE HACERLO							  */
+
 
 
 //setea los vec de la serializadora
@@ -37,13 +40,21 @@ void iniciar_serializadora(){
 	init_vec_deserial();
 }
 
+
 //setea el vector de serializadores
 static void init_vec_serial(){
-	vec_serializador[NOTIF_MOVIMIENTO_PERMITIDO] = srlz_movimiento_permitido;//todo ir agregando a medida que se escriben
+	//todo ir agregando a medida que se escriben
+	vec_serializador[NOTIF_MOVIMIENTO_PERMITIDO] = srlz_movimiento_permitido;
+	vec_serializador[NOTIF_TURNO_CONCLUIDO] = srlz_turno_concluido;
 }
+
+
 //setea el vector de de-serializadores
 static void init_vec_deserial(){
-	vec_deserializador[NOTIF_TURNO_CONCLUIDO] = deserializar_turno_concluido;//todo ir agregando a medida que se escriben
+	//todo ir agregando a medida que se escriben
+	vec_deserializador[NOTIF_TURNO_CONCLUIDO] = deserializar_turno_concluido;
+	vec_deserializador[ENVIO_DE_DATOS_AL_PLANIFICADOR] = deserializar_datos_delPersonaje_alPlanificador;
+	vec_deserializador[NOTIF_MOVIMIENTO_PERMITIDO] = deserializar_movimiento_permitido;
 }
 
 
@@ -69,64 +80,80 @@ int getnextmsg(int socket){
 	return tipo;
 }
 
+
 //lee de un socket y devuelve un struct validando por "tipo"
 void *recibir(int socket, int tipo)
 {
-	char buffer_cabecera[sizeof(t_cabecera)];
+	t_cabecera *cabecera;
+	char *buffer_cuerpo;
+	char *buffer_cabecera = malloc(sizeof(t_cabecera));
 
 	// Primero: Recibir el header para saber cuando ocupa el payload.
-	if (recv(socket, buffer_cabecera, sizeof(t_cabecera), MSG_WAITALL) <= 0) {
+	if (recv(socket, buffer_cabecera, sizeof(t_cabecera) - 1, MSG_WAITALL) <= 0) {
 		/*todo logear*/
 		exit(1);
 	}
 
 	// Segundo: Alocar memoria suficiente para el payload.
-	t_cabecera cabecera = deserializar_cabecera(buffer_cabecera);
-	char buffer_cuerpo[cabecera.len];
+	cabecera = deserializar_cabecera(buffer_cabecera);
+	buffer_cuerpo = malloc(cabecera->len);
 
 	// Tercero: Recibir el payload.
-	if(recv(socket, buffer_cuerpo, cabecera.len, MSG_WAITALL) < 0){
+	if(recv(socket, buffer_cuerpo, cabecera->len, MSG_WAITALL) < 0){
 		/*todo logear*/;
 		exit(1);
 	}
 
-	return cabecera.tipo==tipo? vec_deserializador[tipo](buffer_cuerpo) : NULL ;
+	//de-serializado y validacion de tipo
+	void *deserializado = (*vec_deserializador[tipo])(buffer_cuerpo);
+	bool tipo_valido = cabecera->tipo==tipo;
+
+	free(cabecera);
+	free(buffer_cuerpo);
+	free(buffer_cabecera);
+
+	return tipo_valido? deserializado : NULL ;
 }
+
 
 //funcion que recibe un socket, un tipo de mensaje, un struct y un logger
 //envia un mensaje de un tipo serializando un struct al socket
 void enviar(int socket, int tipo, void *struct_mensaje, t_log *logger)
 {
-	int tmp,offset = 0;
-	char *serializado = vec_serializador[tipo](struct_mensaje);
-	char buffer[sizeof(t_cabecera)+strlen(serializado)];
+	int tmp = 0,offset = 0;
+	char *serializado = (*vec_serializador[tipo])(struct_mensaje);//contiene el serializado correspondiente dado por el "vec"
+	char *buffer = malloc(sizeof(t_cabecera)+strlen(serializado));//buffer de envio del tamaño de la cabecera + el serializado
 
 	//variables para castear
 	uint16_t len_cast = strlen(serializado);
 	uint8_t tipo_cast = tipo;
 
 	//armado del buffer  con cabecera y lo serializado por vec_serializador
-	memmove(buffer, &tipo_cast, tmp = sizeof(uint8_t));
-	memmove(buffer + offset, &len_cast, tmp = sizeof(uint16_t));
+	memcpy(buffer, &tipo_cast, tmp = sizeof(uint8_t));
 	offset += tmp;
-	memmove(buffer + offset, serializado, strlen(serializado));
+	memcpy(buffer + offset, &len_cast, tmp = sizeof(uint16_t));
+	offset += tmp;
+	memcpy(buffer + offset -1, serializado, strlen(serializado));
 
-	//envio de mensaje y limpieza de mallocs
+	//envio de mensaje y checkeo de errores
 	if(send(socket, buffer, strlen(buffer), 0) < 0){
 		/*todo: logear*/
 		exit(1);
 	}
+	//liberacion de mallocs
 	free(struct_mensaje);
 	free(serializado);
+	free(buffer);
 }
+
 
 //OJO ACA:  deserializar cabecera no es parte del vector de de-serializacion y se usa en recibir
 //			no necesitamos tenerla en memoria ni pasarla como void*
-t_cabecera deserializar_cabecera(char *buffer){
-	t_cabecera cabecera;
+t_cabecera *deserializar_cabecera(char *buffer){
+	t_cabecera *cabecera = malloc(sizeof(t_cabecera));
 
-	memcpy(&cabecera.tipo, buffer, sizeof(uint8_t));
-	memcpy(&cabecera.len, buffer, sizeof(uint16_t));
+	memcpy(&cabecera->tipo, buffer, sizeof(uint8_t));
+	memcpy(&cabecera->len, buffer + sizeof(uint8_t), sizeof(uint16_t));
 
 	return cabecera;
 }
@@ -139,29 +166,63 @@ t_cabecera deserializar_cabecera(char *buffer){
 
 
 void *deserializar_turno_concluido(char *buffer){
-	int tmp,offset = 0;
+	int tmp = 0,offset = 0;
 	t_turno_concluido *turno_fin = malloc(sizeof(t_turno_concluido));
 
-	memcpy(&turno_fin->bloqueado, buffer, offset = sizeof(uint8_t));
-	memcpy(&turno_fin->caracter, buffer + offset, tmp = sizeof(uint8_t));
+	memcpy(&turno_fin->bloqueado, buffer, tmp = sizeof(uint8_t));
 	offset += tmp;
-	memcpy(&turno_fin->termino, buffer + offset, sizeof(uint8_t));
+	memcpy(&turno_fin->termino_nivel, buffer + offset, tmp = sizeof(uint8_t));
+	offset += tmp;
+	memcpy(&turno_fin->recurso_de_bloqueo, buffer + offset, sizeof(uint8_t));
 
 	return turno_fin;
 }
 
 
+void *deserializar_datos_delPersonaje_alPlanificador(char *buffer){
+	t_datos_delPersonaje_alPlanificador *datos = malloc(sizeof(t_datos_delPersonaje_alPlanificador));
+
+	memcpy(&datos->char_personaje, buffer, sizeof(uint8_t));
+
+	return datos;
+}
+
+
+void *deserializar_movimiento_permitido(char *buffer){
+	t_mov_permitido *mov_permitido = malloc(sizeof(t_mov_permitido));
+
+	memcpy(&mov_permitido->permitido, buffer, sizeof(t_mov_permitido));
+
+	return mov_permitido;
+}
+
+
 
 /********************************************** FUNCIONES SERIALIZADORAS *********************************************/
-/*  LOS MALLOCS SE LIBERAN EN LA FUNCION ENVIAR, TODOS: LOS DEL BUFFER Y LOS PASADOS COMO ESTRUCT PORQUE SE USAN AHI */
+/*  LOS MALLOCS SE LIBERAN EN LA FUNCION ENVIAR, TODOS: LOS DEL BUFFER Y LOS PASADOS COMO STRUCT PORQUE SE USAN AHI  */
 
 
 
 char *srlz_movimiento_permitido(void* data){
-	t_mov_permitido *temp = data;
+	t_mov_permitido *d = data;
 	char *buffer = malloc(sizeof(uint8_t));
 
-	memcpy(buffer, &temp->permitido, sizeof(uint8_t));
+	memcpy(buffer, &d->permitido, sizeof(uint8_t));
+
+	return buffer;
+}
+
+
+char *srlz_turno_concluido(void *data){
+	int tmp = 0 ,offset = 0;
+	t_turno_concluido *d = data;
+	char *buffer = malloc(3*sizeof(uint8_t));
+
+	memcpy(buffer, &d->bloqueado, tmp = sizeof(uint8_t));
+	offset += tmp;
+	memcpy(buffer + offset, &d->termino_nivel, tmp = sizeof(uint8_t));
+	offset += tmp;
+	memcpy(buffer + offset, &d->recurso_de_bloqueo, sizeof(uint8_t));
 
 	return buffer;
 }
@@ -190,6 +251,8 @@ int init_socket_externo(int puerto, char *direccion, t_log *logger)
 	socketInfo.sin_family = AF_INET;
 	socketInfo.sin_addr.s_addr = inet_addr(direccion);
 	socketInfo.sin_port = htons(puerto);
+
+	connect(unSocket, (struct sockaddr*) &socketInfo, sizeof(socketInfo));
 
 	return unSocket;
 }
--- so-serial-library/serial.h
+++ so-serial-library/serial.h
@@ -5,11 +5,15 @@
  *      Author: julian
  */
 
+#include <stdint.h>
+#include "commons/log.h"
+
 #ifndef SERIAL_H_
 #define SERIAL_H_
 
-	#define NOTIF_MOVIMIENTO_PERMITIDO 3
-	#define NOTIF_TURNO_CONCLUIDO 9
+	#define NOTIF_MOVIMIENTO_PERMITIDO 3				//HP->PP
+	#define NOTIF_TURNO_CONCLUIDO 9						//PP->HP
+	#define ENVIO_DE_DATOS_AL_PLANIFICADOR 19			//PP->HP
 
 	//typedefs de punteros a funciones
 	typedef void *(*p_funcion_deserial)(char *buffer);
@@ -18,19 +22,25 @@
 
 	/**************************** STRUCTS  ***************************/
 
+
+	//struct del mensaje que envia el personaje al planificador cuando conecta por primera vez
+	typedef struct {
+		uint8_t char_personaje;
+	} __attribute__((packed)) t_datos_delPersonaje_alPlanificador;
+
 	//struct simbolico del mensaje movimiento permitido(no se usa)
 	typedef struct {
 		uint8_t permitido;
 	} __attribute__((packed)) t_mov_permitido;
 
-	// struct del mensaje turno concluido
+	//struct del mensaje turno concluido
 	typedef struct{
-		uint8_t bloqueado;
-		uint8_t caracter;
-		uint8_t termino;
+		uint8_t bloqueado ;// usarlo como booleano
+		uint8_t termino_nivel; ;// usarlo como booleano
+		uint8_t recurso_de_bloqueo;
 	} __attribute__((packed)) t_turno_concluido;
 
-	// la cabecera que se lee en todos los mensajes
+	//la cabecera que se lee en todos los mensajes
 	typedef struct {
 			uint8_t tipo;
 			uint16_t len;
@@ -39,17 +49,21 @@
 
 	/**************************** FUNCIONES  ***************************/
 
+
 	//funciones serializadoras
+	char *srlz_turno_concluido(void *data);
 	char *srlz_movimiento_permitido(void* data);
 
 	//funciones de-serializadoras
+	void *deserializar_movimiento_permitido(char *buffer);
 	void *deserializar_turno_concluido(char *buffer);
+	void *deserializar_datos_delPersonaje_alPlanificador(char *buffer);
 
 	//funciones de envio/recepcion
 	int getnextmsg(int socket);
 	void *recibir(int socket, int tipo);
 	void enviar(int socket, int tipo, void *struct_mensaje, t_log *logger);
-	t_cabecera deserializar_cabecera(char *buffer);
+	t_cabecera *deserializar_cabecera(char *buffer);
 
 	//funciones de socket
 	int init_socket_externo(int puerto, char *direccion, t_log *logger);
