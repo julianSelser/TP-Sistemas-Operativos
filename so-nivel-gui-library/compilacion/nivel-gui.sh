952de1ba3c0fef4e37ad9e3ca725a05562ac49e0
dcappellini <dcappellini@hotmail.com.ar>
2013-07-13 03:36:33 -0300

    Cambio los scripts de compilacion para que tengan path relativo.

--- so-nivel-gui-library/compilacion/nivel-gui.sh
+++ so-nivel-gui-library/compilacion/nivel-gui.sh
@@ -1,5 +1,5 @@
 mkdir nivel-gui
 
-gcc -I/usr/include -I/usr/include/i386-linux-gnu -I/usr/lib/gcc/i686-linux-gnu/4.7/include -I/usr/lib/gcc/i686-linux-gnu/4.7/include-fixed -I/usr/local/include -I"/home/utnso/git/tp-20131c-juanito-y-los-clonosaurios/so-commons-library/src" -I"/home/utnso/git/tp-20131c-juanito-y-los-clonosaurios/so-nivel-gui-library/nivel-gui" -I"/home/utnso/git/tp-20131c-juanito-y-los-clonosaurios/tad_items" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"nivel-gui/nivel.d" -MT"nivel-gui/nivel.d" -o "nivel-gui/nivel.o" "../nivel-gui/nivel.c"
+gcc -I/usr/include -I/usr/include/i386-linux-gnu -I/usr/lib/gcc/i686-linux-gnu/4.7/include -I/usr/lib/gcc/i686-linux-gnu/4.7/include-fixed -I/usr/local/include -I"../../so-commons-library/src" -I"../../so-nivel-gui-library/nivel-gui" -I"../../tad_items" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"nivel-gui/nivel.d" -MT"nivel-gui/nivel.d" -o "nivel-gui/nivel.o" "../nivel-gui/nivel.c"
 
 g++ -shared -o "libso-nivel-gui-library.so"  ./nivel-gui/nivel.o   
